16627ae530b9152f182810dcd3987891e338b6a3
VeronikaYuzikhub <veronikauzik478@gmail.com>
2026-06-16 11:27:06 +0200

    MVP Initiation: Back-End and Front-End

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "MVP Back-End is working! ");
+app.MapGet("/", () => "MVP Back-End is working!");
 
 app.Run();
