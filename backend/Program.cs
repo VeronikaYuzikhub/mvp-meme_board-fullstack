2249958f34e4323ecfa434adbd302527f7aca92c
VeronikaYuzikhub <veronikauzik478@gmail.com>
2026-06-30 01:45:32 +0200

    [fix] include SQLite db in Docker publish

--- backend/Program.cs
+++ backend/Program.cs
@@ -93,6 +93,12 @@ builder.Services.AddSwaggerGen(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseCors();
 
 //генерація опису (джсон) та інтерактивної сторінки SwaggerUI
